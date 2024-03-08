b98338b21fab3ab79d0d5530e96b3a4074c2b40c
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-08 17:26:24 -0500

    Minor patch

--- libz/flood.c
+++ libz/flood.c
@@ -1,3 +1,8 @@
+/*
+This program is licensed under the GNU General Public License v3.
+Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+See https://www.gnu.org/licenses/gpl-3.0.html for details.
+*/
 #ifdef __WIN32__
 
 #error "Using this script on the WINDOWS is not supported. Please use Windows directory. "
